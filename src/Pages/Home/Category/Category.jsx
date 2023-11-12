8d9cdfdcaedc89201c4a812f0f187ca3c57d7a65
Md Shawon Reza <fahadshawon241@gmail.com>
2023-11-13 05:38:24 +0600

    add this section title

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -8,42 +8,49 @@ import slide2 from "../../../assets/home/slide2.jpg"
 import slide3 from "../../../assets/home/slide3.jpg"
 import slide4 from "../../../assets/home/slide4.jpg"
 import slide5 from "../../../assets/home/slide5.jpg"
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const Category = () => {
 
     return (
-        <Swiper
-            slidesPerView={4}
-            spaceBetween={30}
-            centeredSlides={true}
-            pagination={{
-                clickable: true,
-            }}
-            modules={[Pagination]}
-            className="mySwiper mb-8 md:mb-16"
-        >
-            <SwiperSlide>
-                <img className='relative ' src={slide1} alt="" />
-                <h2 className='absolute uppercase w-full text-center -mt-8 md:-mt-16 text-gray-100 text-base md:text-2xl font-semibold'>Salads</h2>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img className='relative ' src={slide2} alt="" />
-                <h2 className='absolute uppercase w-full text-center -mt-8 md:-mt-16 text-gray-100 text-base md:text-2xl font-semibold'> Soups</h2>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img className='relative ' src={slide3} alt="" />
-                <h2 className='absolute uppercase w-full text-center -mt-8 md:-mt-16 text-gray-100 text-base md:text-2xl font-semibold'>pizzas</h2>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img className='relative ' src={slide4} alt="" />
-                <h2 className='absolute uppercase w-full text-center -mt-8 md:-mt-16 text-gray-100 text-base md:text-2xl font-semibold'>desserts</h2> 
-            </SwiperSlide>
-            <SwiperSlide>
-                <img className='relative ' src={slide5} alt="" />
-                <h2 className='absolute uppercase w-full text-center -mt-8 md:-mt-16 text-gray-100 text-base md:text-2xl font-semibold'>Salads</h2>
-            </SwiperSlide>
-           
-        </Swiper>
+        <section className=''>
+            <SectionTitle subHeading={"From 11:00am to 10:00pm"} heading={"ORDER ONLINE"}>
+                
+            </SectionTitle>
+
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                centeredSlides={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper mb-8 md:mb-16  "
+            >
+                <SwiperSlide>
+                    <img className='relative ' src={slide1} alt="" />
+                    <h2 className='absolute uppercase w-full text-center -mt-8 md:-mt-16 text-gray-100 text-base md:text-2xl font-semibold'>Salads</h2>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='relative ' src={slide2} alt="" />
+                    <h2 className='absolute uppercase w-full text-center -mt-8 md:-mt-16 text-gray-100 text-base md:text-2xl font-semibold'> Soups</h2>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='relative ' src={slide3} alt="" />
+                    <h2 className='absolute uppercase w-full text-center -mt-8 md:-mt-16 text-gray-100 text-base md:text-2xl font-semibold'>pizzas</h2>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='relative ' src={slide4} alt="" />
+                    <h2 className='absolute uppercase w-full text-center -mt-8 md:-mt-16 text-gray-100 text-base md:text-2xl font-semibold'>desserts</h2>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='relative ' src={slide5} alt="" />
+                    <h2 className='absolute uppercase w-full text-center -mt-8 md:-mt-16 text-gray-100 text-base md:text-2xl font-semibold'>Salads</h2>
+                </SwiperSlide>
+
+            </Swiper>
+        </section>
     );
 };
 
---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,11 @@
+
+const SectionTitle = ({subHeading,heading}) => {
+    return (
+        <div className="text-center  md:w-3/12 mx-auto my-10">
+            <p className="text-[#D99904] text-lg mb-2">--- {subHeading} ---</p>
+            <h2 className="uppercase text-4xl font-semibold py-3 border-y-2">{heading}</h2>
+        </div>
+    );
+};
+
+export default SectionTitle;
